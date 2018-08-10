76a1d8f0af2fe41c524cc0f2199003d1654396a1
Warren Usui <wusui@magna002.ceph.redhat.com>
2018-08-10 19:56:01 -0400

    Make server runner.sh not dependent on secret file. If secret file exists, use it to handle RHEL subscriptions

--- server/README.md
+++ server/README.md
@@ -4,8 +4,11 @@ Install an IPA server on a machine in order to test ldap authentication of
 Rados Gateway users.
 
 To Run:
+```
+./runner.sh HOST
+```
 
-create a file named secrets that minimally contains the following lines: 
+If you want to install using the subscription manager to setup a RHEL system, before running runner.sh create a file named secrets that minimally contains the following lines:
 
 ```
 export subscrname='USER@redhat.com'
@@ -14,9 +17,3 @@ export subscrpassword='PASSWORD'
 
 USER is a Red Hat subscription-manager user.
 PASSWORD is USER's password.
-
-To run:
-
-```
-./runner.sh HOST
-```
--- server/runner.sh
+++ server/runner.sh
@@ -1,18 +1,22 @@
 #!/bin/bash -fv
+function cpy() {
+    echo $1
+    scp /tmp/ipatar $1:/tmp/ipatar
+    scp unpacker.sh $1:/tmp/unpacker.sh
+    ssh $1 sudo /tmp/unpacker.sh
+}
+
 ./paranoia.sh $1
 if $retv; then
     tar cvf /tmp/ipatar .
     iyam=`whoami`
-    scp /tmp/ipatar $iyam@$1:/tmp/ipatar
-    scp unpacker.sh $iyam@$1:/tmp/unpacker.sh
-    ssh $iyam@$1 sudo /tmp/unpacker.sh
-    ssh $iyam@$1 sudo /tmp/ipasetup/subscribe.sh
-    rm -fr /tmp/ipasetup/secrets
-    sleep 240
-    scp /tmp/ipatar $iyam@$1:/tmp/ipatar
-    scp unpacker.sh $iyam@$1:/tmp/unpacker.sh
-    rm -fr /tmp/ipasetup/secrets
-    ssh $iyam@$1 sudo /tmp/unpacker.sh
+    if [ -f secrets ]; then
+        cpy $iyam@$1
+        ssh $iyam@$1 sudo /tmp/ipasetup/subscribe.sh
+        sleep 240
+    fi
+    cpy $iyam@$1
+    ssh $iyam@$1 rm -fr /tmp/ipasetup/secrets
     ssh $iyam@$1 sudo /tmp/ipasetup/ipasetup.sh
     ssh $iyam@$1 sudo /tmp/ipasetup/useradd.sh
 fi
