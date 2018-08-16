15d5787c7c9ca69c6b5d096a472d5d0135a5a60a
Warren Usui <wusui@magna002.ceph.redhat.com>
2018-08-15 22:03:21 -0400

    Made install command more general.  Moved up one directory

--- install_server.sh
+++ install_server.sh
@@ -10,13 +10,12 @@ function cpy() {
 if $retv; then
     tar cvf /tmp/ipatar .
     iyam=`whoami`
+    cpy $iyam@$1
     if [ -f secrets ]; then
-        cpy $iyam@$1
         ssh $iyam@$1 sudo /tmp/ipasetup/subscribe.sh
         sleep 240
     fi
-    cpy $iyam@$1
     ssh $iyam@$1 rm -fr /tmp/ipasetup/secrets
-    ssh $iyam@$1 sudo /tmp/ipasetup/ipasetup.sh
-    ssh $iyam@$1 sudo /tmp/ipasetup/useradd.sh
+    ssh $iyam@$1 sudo /tmp/ipasetup/server/ipasetup.sh
+    ssh $iyam@$1 sudo /tmp/ipasetup/server/useradd.sh
 fi
--- server/ipasetup.sh
+++ server/ipasetup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -fv
+iptables -F
 sudo yum -y install ipa-server ipa-server-dns
 sudo yum -y remove mod_ssl
+sudo systemctl restart messagebus
 sudo ipa-server-install --realm ceph.redhat.com --ds-password t0pSecret --admin-password t0pSecret --unattended
-iptables -F
